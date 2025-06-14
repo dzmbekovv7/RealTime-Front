2dc1af62f5aae83c821f0b68ae969b746182a5d5
Wizqqr <132731212+Wizqqr@users.noreply.github.com>
2025-06-14 17:41:10 +0600

    server

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -2,8 +2,8 @@
 import axios from "axios";
 
 const BASE_URL = import.meta.env.MODE === "development"
-  ? "http://localhost:5001"
-  : "https://realtime-back.onrender.com";
+  ? "http://localhost:5001/api"
+  : "https://realtime-back.onrender.com/api";
 
 export const axiosInstance = axios.create({
   baseURL: BASE_URL,
