36e2a50ad73bc851403ce9eacd2855027c542e4a
Wizqqr <132731212+Wizqqr@users.noreply.github.com>
2025-06-14 17:31:09 +0600

    server

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
-  withCredentials: true,
+	baseURL: import.meta.env.MODE === "development"
+		? "http://localhost:5001/api"
+		: "https://realtime-back.onrender.com/api",
+	withCredentials: true,
 });
-
